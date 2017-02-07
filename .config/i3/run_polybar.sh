f80cb68960c80aaf4f5e834feb8b147024ba0688
Nicholas Jones <dojoman19@gmail.com>
2017-02-07 17:43:11 -0500

    Deleted pointless run_polybar.sh

--- .config/i3/run_polybar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-killall polybar
-polybar bar1 -c /home/zedjones/.polybar &
-polybar bar2 -c /home/zedjones/.polybar &
-polybar topbar1 -c /home/zedjones/.polybar &
-polybar topbar2 -c /home/zedjones/.polybar &
