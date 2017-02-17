bb5bdc60e319e2827ef8a70530d40273987d9388
Nicholas <dojoman19@gmail.com>
2017-02-17 15:52:50 -0500

    Fixed rofi scripts to be resolution agnostic

--- .config/i3/rofi_dmenu.sh
+++ .config/i3/rofi_dmenu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rofi -dmenu -width 2560 -fg "#B0E670" -bg "#14161F" -hlfg "#14161F" -hlbg "#B0E670" -bc "#14161F" -location 0 -lines 19 -bw 0 -font "MonacoB 16" -padding 770 -separator-style none -opacity 80 -hide-scrollbar
+rofi -dmenu -fullscreen -fg "#B0E670" -bg "#14161F" -hlfg "#14161F" -hlbg "#B0E670" -bc "#14161F" -location 0 -lines 19 -bw 0 -font "MonacoB 16" -padding 350 -separator-style none -opacity 80 -hide-scrollbar
