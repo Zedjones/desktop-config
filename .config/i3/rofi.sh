9654f14000c4c3efcc16ae849378d68a3758de89
Nicholas <dojoman19@gmail.com>
2017-02-17 11:38:06 -0500

    Switched to wal and fullscreen rofi, trimmed excess fonts

---
+++ .config/i3/rofi.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rofi -show run -width 2560 -fg "#B0E670" -bg "#14161F" -hlfg "#14161F" -hlbg "#B0E670" -bc "#14161F" -location 0 -lines 19 -bw 0 -font "MonacoB 16" -padding 770 -separator-style none -opacity 80 -hide-scrollbar
