417fbdde4a2284f653af75c554ad75cb4d191684
Nicholas <dojoman19@gmail.com>
2017-04-25 23:15:20 -0400

    Nevermind, that was supposed to be gone

--- .config/i3/run_polybar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sleep 1
-killall polybar
-polybar bar1 -c /home/zedjones/.polybar &
-polybar bar2 -c /home/zedjones/.polybar &
-polybar topbar1 -c /home/zedjones/.polybar &
-polybar topbar2 -c /home/zedjones/.polybar &
