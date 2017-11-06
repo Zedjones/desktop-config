2ec50cc541ababde3adecd5b2a7d7ee25abd7abd
Nicholas <dojoman19@gmail.com>
2017-11-06 12:45:25 -0500

    Changed fonts, added icons to polybar, added notifications via dunst

--- .config/i3/rofi.sh
+++ .config/i3/rofi.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rofi -show run -fullscreen -font "Fira Code 14" -fg "#B0E670" -bg "#14161F" -hlfg "#14161F" -hlbg "#B0E670" -bc "#14161F" -location 0 -lines 19 -bw 0 -font "MonacoB 16" -padding 350 -separator-style none -opacity 80 -hide-scrollbar 
+rofi -show run -fullscreen -font "ypn envypn 11" -fg "#B0E670" -bg "#14161F" -hlfg "#14161F" -hlbg "#B0E670" -bc "#14161F" -location 0 -lines 19 -bw 0 -font "MonacoB 16" -padding 350 -separator-style none -opacity 80 -hide-scrollbar 
